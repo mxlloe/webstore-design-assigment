8775461d61c8aaa652bee16e4bdd6d776fe143ce
Sean Ramolete <62075418+mxlloe@users.noreply.github.com>
2020-08-09 14:54:53 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+/* Add any JavaScript you need to this file. */
+
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
